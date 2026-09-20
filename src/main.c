e8cfe8bffd40783e4b99020bd031ba4e60633931
Samim0101 <samim.py01@gmail.com>
2026-09-21 01:07:13 +0530

    Add account structure and input

--- src/main.c
+++ src/main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
-
+typedef struct
+{
+    int account_number;
+    char name[50];
+    float balance;
+} Account;
 int main()
 {
     printf("====================================\n");
     printf("       BANK MANAGEMENT SYSTEM       \n");
     printf("====================================\n");
+
+    Account account1;
+    printf("Enter your account number: ");
+    scanf("%d",&account1.account_number);
+
+    printf("Enter your name: ");
+    scanf("%s",account1.name);
+
+    printf("Enter your balance: ");
+    scanf("%f",&account1.balance);
+
+    printf("Account Number: %d\n", account1.account_number);
+    printf("Account Name: %s\n", account1.name);
+    printf("Account Balance: %.2f\n", account1.balance);
 
     return 0;
 }
