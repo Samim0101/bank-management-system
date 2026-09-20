bcd22bc6abc3dbe209ab4c3a5cc1d5dfda060057
Samim0101 <samim.py01@gmail.com>
2026-09-21 00:35:04 +0530

    Initial project setup

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("====================================\n");
+    printf("       BANK MANAGEMENT SYSTEM       \n");
+    printf("====================================\n");
+
+    return 0;
+}
