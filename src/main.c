43bd707a3e0bda09f4fe3fc96a30102a21df4741
Samim0101 <samim.py01@gmail.com>
2026-09-21 11:43:48 +0530

    Implement add account function

--- src/main.c
+++ src/main.c
@@ -5,6 +5,21 @@ typedef struct
     char name[50];
     float balance;
 } Account;
+
+void add_account(Account accounts[], int *account_count)
+{
+    printf("Enter your account number: ");
+
+    scanf("%d", &accounts[*account_count].account_number);
+
+    printf("Enter your name: ");
+    scanf("%s", accounts[*account_count].name);
+
+    printf("Enter bank Balance: ");
+    scanf("%f", &accounts[*account_count].balance);
+
+    (*account_count++);
+}
 int main()
 {
     printf("====================================\n");
@@ -14,16 +29,7 @@ int main()
     Account accounts[100];
     int account_count = 0;
 
-    printf("Enter your account number: ");
-    scanf("%d", &accounts[account_count].account_number);
-
-    printf("Enter your name: ");
-    scanf("%s", accounts[account_count].name);
-
-    printf("Enter your balance: ");
-    scanf("%f", &accounts[account_count].balance);
-
-    account_count++;
+    add_account(accounts, &account_count);
 
     printf("Account Number: %d\n", accounts[0].account_number);
     printf("Account Name: %s\n", accounts[0].name);
